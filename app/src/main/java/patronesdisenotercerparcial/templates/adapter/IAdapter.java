95e49c2eb865dc83f91ed4d79b8d76705c7dd03e
Vivian Marino <marino6733784@gmail.com>
2023-03-22 12:29:17 -0400

    creando repo tercer parcial

---
+++ app/src/main/java/patronesdisenotercerparcial/templates/adapter/IAdapter.java
@@ -0,0 +1,13 @@
+package patronesdisenotercerparcial.templates.adapter;
+
+public interface IAdapter {
+
+    // metodos de familia original
+    void method1();
+    void method2();
+    void method3();
+
+    // metodo adaptador
+    void operation();
+
+}
